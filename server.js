9001c14655b3a645450424eb2d021f5c584ddf75
Alden <aldenryabyuk@gmail.com>
2023-03-13 22:26:28 -0500

    Server Commit

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.get('/', (req, res) => {
 
 // Set up your API routes here
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
